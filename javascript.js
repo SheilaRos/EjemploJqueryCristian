a9e77208a2efdddfed7d65bc343332e6efd9a30c
SheilaRos <sheila.ros.cheme@gmail.com>
2017-01-18 21:11:38 +0100

    Ejemplo terminado

--- javascript.js
+++ javascript.js
@@ -9,4 +9,39 @@ function inicio(){
     $("#color").css(
             {"color": "red",
         "background-color": "blue"});
+
+    $("#clickame").click(changeColorDivs);
+    $("#clickame").mouseenter(expandir);
+    $("#clickame").mouseleave(contraer);
+    $(".miDiv").click(changeMiDiv);
+    $("#color").click(newElement);
+}
+
+var numDiv = 0;
+function newElement(){
+    var div = $("<div class='miDiv2'>Nanananana"+numDiv+"</div>");
+    numDiv++;
+    $("#newDiv").append(div);
+    div.click(changeMiDiv);
+    div.dblclick(delPreview);
+}
+
+function delPreview(){
+    $(this).prev().remove();
+}
+function changeColorDivs(){
+    $("div").css({"backgroundColor": "pink"}); 
+}
+
+function expandir(){
+    $("#clickame").animate({"width": "200px", "height": "400px"}, {"duration": "2500"});
+}
+function contraer(){
+    $("#clickame").animate({"width": "50px", "height": "100px"}, {"duration": "2500", complete:changeColor});
+}
+function changeColor(){
+    $("#clickame").css({"background-color":"green"});
+}
+function changeMiDiv(){
+    $(this).css({"background-color":"blue", "margin-left":"+=30px"});
 }
